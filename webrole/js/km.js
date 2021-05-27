9d2e732d1e1977ec9ed0ae4fa9a1983ea37cb6df
David Solsona <daviddsa21@gmail.com>
2021-05-27 17:21:47 +0200

    FIX

--- webrole/js/km.js
+++ webrole/js/km.js
@@ -46,7 +46,7 @@ function hayEncuentro() {
         if (posEncuentro <= siEncuentro + porAcumulado) {
             document.getElementById("encuentro").innerHTML =
             "<br>" + new Date().toLocaleTimeString('es-ES', { hour: "numeric", minute: "numeric", second: "numeric"})
-            + " --> ¡Hay encuentro! [" + posEncuentro + "+" + porAcumulado + "<=" + siEncuentro + " ?]" + infoEncuentro(encuentro, getRadioValue("parte"), getRadioValue("terreno"));
+            + " --> ¡Hay encuentro! [" + posEncuentro + "<=" + porAcumulado + "+" + siEncuentro + " ?]" + infoEncuentro(encuentro, getRadioValue("parte"), getRadioValue("terreno"));
 
             porAcumulado = 0;
         } else {
